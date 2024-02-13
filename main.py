bbb70ea91c95dc44c0558135f6708008e7209e90
Andrés Ingelmo <aingelmop@gmail.com>
2024-02-13 22:32:20 +0100

    Add main and model

---
+++ main.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from typing import Literal
+
+import pandas as pd
+from fastapi import FastAPI
+from pycaret.classification import load_model, predict_model
+from pydantic import BaseModel
+
+
+class Data(BaseModel):
+    Year_Birth: int
+    Education: Literal["Graduation", "PhD", "Master", "2n Cycle", "Basic"]
+    Income: float
+
+
+model_location = Path(__file__).parent / "models" / "gbr_mnt_wine"
+
+app = FastAPI()
+
+model = load_model(str(model_location))
+
+
+@app.post("/predict")
+def predict(input_dict: Data):
+    input_df = pd.DataFrame([input_dict.model_dump()])
+    predictions_df = predict_model(estimator=model, data=input_df)
+    prediction = predictions_df["prediction_label"].iloc[0]
+    return int(prediction)
